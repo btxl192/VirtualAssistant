2bb9b8c62e7b34b10bacc25e911e565e93002cee
brandon tan <btan192@gmail.com>
2020-10-28 22:59:53 +0800

    test

--- API-code/main2.py
+++ API-code/main2.py
@@ -3,46 +3,6 @@ from ask_sdk_core.skill_builder import SkillBuilder
 from flask_ask_sdk.skill_adapter import SkillAdapter
 from copy import deepcopy
 
-json_response_template = {
-    "version":"1.0",
-    # "sessionAttributes": {
-
-    # },
-    "response":{
-        "outputSpeech":{
-            #https://developer.amazon.com/docs/custom-skills/request-and-response-json-reference.html#outputspeech-object
-            "type":"PlainText",
-        #    "text":"text to output from alexa",
-            "playBehaviour":"REPLACE_ENQUEUED"
-        },
-    #  "reprompt": {
-    #   "outputSpeech": {
-    #     "type": "PlainText",
-    #     "text": "Plain text string to speak",
-    #     "playBehavior": "REPLACE_ENQUEUED"             
-    #   }
-    # },
-        # "directives": [
-        # ],
-        "shouldEndSession": False,
-    }
-}
-
-def text_response(text):
-    response = deepcopy(json_response_template)
-    response["response"]["outputSpeech"]["text"] = text
-    return response
-
-app = Flask(__name__)
-skill_builder = SkillBuilder()
-# Register your intent handlers to the skill_builder object
-
-skill_adapter = SkillAdapter(skill=skill_builder.create(), skill_id="1", app=app)
-
-@app.route("/api/v1/blueassistant", methods=['POST'])
-def invoke_skill():
-    return skill_adapter.dispatch_request()
-
 from ask_sdk_core.dispatch_components import AbstractRequestHandler
 from ask_sdk_core.utils import is_intent_name
 from ask_sdk_model.ui import SimpleCard
@@ -67,4 +27,15 @@ class LaunchRequestHandler(AbstractRequestHandler):
             .response
         )
 
+app = Flask(__name__)
+skill_builder = SkillBuilder()
+# Register your intent handlers to the skill_builder object
+
+#skill_adapter = SkillAdapter(skill=skill_builder.create(), skill_id="1", app=app)
+
 skill_builder.add_request_handler(LaunchRequestHandler())
+
+@app.route("/api/v1/blueassistant", methods=['POST'])
+def invoke_skill():
+    s = SkillAdapter(skill=LaunchRequestHandler, skill_id="1", app=app)
+    return s
