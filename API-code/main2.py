04af29bb99610b94f2bc8c618451db1b58255c19
brandon tan <btan192@gmail.com>
2020-10-28 23:08:44 +0800

    added session ended request

--- API-code/main2.py
+++ API-code/main2.py
@@ -1,33 +1,62 @@
-import logging
-import os
-
 from flask import Flask
-from flask_ask import Ask, request, session, question, statement
+from ask_sdk_core.skill_builder import SkillBuilder
+from flask_ask_sdk.skill_adapter import SkillAdapter
+from copy import deepcopy
 
+from ask_sdk_core.dispatch_components import AbstractRequestHandler
+from ask_sdk_core.utils import is_intent_name
+from ask_sdk_model.ui import SimpleCard
+
+class LaunchRequestHandler(AbstractRequestHandler):
+    """Handler for Skill Launch."""
+
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+
+        return ask_utils.is_request_type("LaunchRequest")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        _ = handler_input.attributes_manager.request_attributes["_"]
+        speak_output = _("test")
+
+        return (
+            handler_input.response_builder
+            .speak(speak_output)
+            .ask(speak_output)
+            .response
+        )
+
+class SessionEndedRequest(AbstractRequestHandler):
+    """Handler for Skill Launch."""
+
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+
+        return ask_utils.is_request_type("SessionEndedRequest")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        _ = handler_input.attributes_manager.request_attributes["_"]
+        speak_output = _("test2")
+
+        return (
+            handler_input.response_builder
+            .speak(speak_output)
+            .ask(speak_output)
+            .response
+        )
 
 app = Flask(__name__)
-ask = Ask(app, "/api/blueassistant")
-logging.getLogger('flask_ask').setLevel(logging.DEBUG)
+skill_builder = SkillBuilder()
+# Register your intent handlers to the skill_builder object
 
+#skill_adapter = SkillAdapter(skill=skill_builder.create(), skill_id="1", app=app)
 
-@ask.launch
-def launch():
-    speech_text = 'Welcome to the Alexa Skills Kit, you can say hello'
-    return question(speech_text).reprompt(speech_text).simple_card('HelloWorld', speech_text)
+skill_builder.add_request_handler(LaunchRequestHandler())
+skill_builder.add_request_handler(SessionEndedRequest())
 
-
-@ask.intent('HelloWorldIntent')
-def hello_world():
-    speech_text = 'Hello world'
-    return statement(speech_text).simple_card('HelloWorld', speech_text)
-
-
-@ask.intent('AMAZON.HelpIntent')
-def help():
-    speech_text = 'You can say hello to me!'
-    return question(speech_text).reprompt(speech_text).simple_card('HelloWorld', speech_text)
-
-
-@ask.session_ended
-def session_ended():
-    return "{}", 200
+@app.route("/api/v1/blueassistant", methods=['POST'])
+def invoke_skill():
+    s = SkillAdapter(skill=LaunchRequestHandler, skill_id="1", app=app)
+    return s
