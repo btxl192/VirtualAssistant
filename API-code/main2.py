ab57f55ddd3585cb73263ee015170eb639dde55c
brandon tan <btan192@gmail.com>
2020-10-28 22:08:28 +0800

    upload

---
+++ API-code/main2.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from ask_sdk_core.skill_builder import SkillBuilder
+from flask_ask_sdk.skill_adapter import SkillAdapter
+
+app = Flask(__name__)
+skill_builder = SkillBuilder()
+# Register your intent handlers to the skill_builder object
+
+skill_adapter = SkillAdapter(
+    skill=skill_builder.create(), skill_id="1", app=app)
+
+@app.route("/api/v1/blueassistant")
+def invoke_skill():
+    return skill_adapter.dispatch_request()
+
+app.run()
