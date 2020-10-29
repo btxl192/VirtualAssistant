db437a6bfbee86d7b8263317474da2b3e81f8284
brandon tan <btan192@gmail.com>
2020-10-29 15:58:05 +0800

    edited alexa response

--- API-code/main2.py
+++ API-code/main2.py
@@ -4,37 +4,32 @@ from flask_ask_sdk.skill_adapter import SkillAdapter
 from copy import deepcopy
 
 from ask_sdk_core.dispatch_components import AbstractRequestHandler
-from ask_sdk_core.utils import is_intent_name
+from ask_sdk_core.utils import is_intent_name, is_request_type
 from ask_sdk_model.ui import SimpleCard
-import ask_sdk_core.utils as ask_utils
 
 class LaunchRequestHandler(AbstractRequestHandler):
     """Handler for Skill Launch."""
-
     def can_handle(self, handler_input):
         # type: (HandlerInput) -> bool
-
-        return ask_utils.is_request_type("LaunchRequest")(handler_input)
+        return is_request_type("LaunchRequest")(handler_input)
 
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
-        speech_text = "test"
-
-        return handler_input.response_builder.speak(speech_text).set_card(SimpleCard("Hello World", speech_text)).response
+        speech_text = "Hi, welcome to Blue, your personal lab assistant. How may I help you today?"
+        return handler_input.response_builder.speak(speech_text).response
 
 class SessionEndedRequest(AbstractRequestHandler):
     """Handler for Skill End."""
 
     def can_handle(self, handler_input):
         # type: (HandlerInput) -> bool
-
-        return ask_utils.is_request_type("SessionEndedRequest")(handler_input)
+        return is_request_type("SessionEndedRequest")(handler_input)
 
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
         speech_text = "test2"
 
-        return handler_input.response_builder.speak(speech_text).set_card(SimpleCard("Hello World", speech_text)).response
+        return handler_input.response_builder.speak(speech_text).response
 
 app = Flask(__name__)
 skill_builder = SkillBuilder()
