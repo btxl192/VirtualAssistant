6d35ac54f74a58c8a87748667235701b5a95fb57
brandon tan <btan192@gmail.com>
2020-12-08 16:40:31 +0800

    sends the response to unity

--- API-code/handled_intents/intent_base.py
+++ API-code/handled_intents/intent_base.py
@@ -26,6 +26,7 @@ class intent_base(AbstractRequestHandler):
 
     def handle(self, handler_input):
         self.action(handler_input.request_envelope.request.intent)
+        self.push_to_notifier("AlexaResponse: " + self.response)
         return handler_input.response_builder.speak(self.response).set_should_end_session(self.should_end_session).response
 
     #Sends a message through the websocket to the Unity client
