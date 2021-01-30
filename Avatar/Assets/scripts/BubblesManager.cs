02ad0bf791a852d95e7e1011b681d1937833884d
Kaloyan Rusev <kaloyan.rusev.19@ucl.ac.uk>
2021-01-30 12:25:32 +0200

    added handlers for the speech bubbles

--- Avatar/Assets/scripts/BubblesManager.cs
+++ Avatar/Assets/scripts/BubblesManager.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿using System;
+using System.Globalization;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
@@ -7,7 +9,7 @@ public class BubblesManager : MonoBehaviour
 {
     [SerializeField] private float typingSpeed = 0.01f;
 
-    [Header("Bubbles TMP text")]
+    [Header("Bubbles TMP")]
     [SerializeField] private TextMeshProUGUI bubbleLeftUp;
     [SerializeField] private TextMeshProUGUI bubbleLeftDown;
     [SerializeField] private TextMeshProUGUI bubbleRightUp;
@@ -31,7 +33,12 @@ public class BubblesManager : MonoBehaviour
 
     private float bubbleAnimationDelay = 0.6f;
 
-    private IEnumerator typeLeftUpText(){
+    private player thisplayer;
+    private bool areDisplayed;
+    DateTime timeLastSpoke;
+
+    private IEnumerator typeLeftUpText()
+    {
         foreach (char letter in bubbleLeftUpText.ToCharArray())
         {
             bubbleLeftUp.text += letter;
@@ -39,7 +46,8 @@ public class BubblesManager : MonoBehaviour
         }
     }
 
-    private IEnumerator typeLeftDownText(){
+    private IEnumerator typeLeftDownText()
+    {
         foreach (char letter in bubbleLeftDownText.ToCharArray())
         {
             bubbleLeftDown.text += letter;
@@ -47,7 +55,8 @@ public class BubblesManager : MonoBehaviour
         }
     }
 
-    private IEnumerator typeRightUpText(){
+    private IEnumerator typeRightUpText()
+    {
         foreach (char letter in bubbleRightUpText.ToCharArray())
         {
             bubbleRightUp.text += letter;
@@ -55,7 +64,8 @@ public class BubblesManager : MonoBehaviour
         }
     }
 
-    private IEnumerator typeRightDownText(){
+    private IEnumerator typeRightDownText()
+    {
         foreach (char letter in bubbleRightDownText.ToCharArray())
         {
             bubbleRightDown.text += letter;
@@ -67,9 +77,25 @@ public class BubblesManager : MonoBehaviour
         bubbleLeftUp.text = string.Empty;
     }
 
-    private IEnumerator openBubbles(){
+    private IEnumerator closeBubbles()
+    {
+        bubbleRightUpAnimator.SetTrigger("Close");
+        bubbleLeftUpAnimator.SetTrigger("Close");
+        bubbleRightDownAnimator.SetTrigger("Close");
+        bubbleLeftDownAnimator.SetTrigger("Close");
 
-        yield return new WaitForSeconds(2);
+        bubbleLeftUp.text = string.Empty;
+        bubbleLeftDown.text = string.Empty;
+        bubbleRightUp.text = string.Empty;
+        bubbleRightDown.text = string.Empty;
+
+        yield return new WaitForSeconds(bubbleAnimationDelay);
+    }
+
+    private IEnumerator openBubbles()
+    {
+
+        yield return new WaitForSeconds(1);
 
         bubbleRightUpAnimator.SetTrigger("Open");
         bubbleLeftUpAnimator.SetTrigger("Open");
@@ -83,26 +109,33 @@ public class BubblesManager : MonoBehaviour
         StartCoroutine(typeRightDownText());
         StartCoroutine(typeLeftDownText());
 
-        yield return new WaitForSeconds(10);
-
-        bubbleRightUpAnimator.SetTrigger("Close");
-        bubbleLeftUpAnimator.SetTrigger("Close");
-        bubbleRightDownAnimator.SetTrigger("Close");
-        bubbleLeftDownAnimator.SetTrigger("Close");
-
-        bubbleLeftUp.text = string.Empty;
-        bubbleLeftDown.text = string.Empty;
-        bubbleRightUp.text = string.Empty;
-        bubbleRightDown.text = string.Empty;
-
-        yield return new WaitForSeconds(bubbleAnimationDelay);
-
-
     }
 
     // Start is called before the first frame update
     void Start()
     {
+        thisplayer = GetComponent<player>();
+        timeLastSpoke = DateTime.Now;
         StartCoroutine(openBubbles());
+        areDisplayed = true;
+    }
+
+     // Update is called once per frame
+    void Update()
+    {
+        DateTime now = DateTime.Now;
+        double seconds = (now - timeLastSpoke).TotalSeconds;
+        bool isTalking = thisplayer.isTalking;
+        if(isTalking){
+            if(areDisplayed){
+                StartCoroutine(closeBubbles());
+                areDisplayed = false;
+            }
+            timeLastSpoke = DateTime.Now;
+        }
+        else if (seconds > 30){
+            StartCoroutine(openBubbles());
+            areDisplayed = true;
+        }
     }
 }
