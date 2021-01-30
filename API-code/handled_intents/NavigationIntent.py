97ed7e450a4ac96e8b9ee8fb1d4eae13b906cc91
Kaloyan Rusev <kaloyan.rusev.19@ucl.ac.uk>
2021-01-30 12:26:29 +0200

    added a handler for the navigation intent

---
+++ API-code/handled_intents/NavigationIntent.py
@@ -0,0 +1,25 @@
+from .intent_base import *
+import json
+
+def get_route(room):
+	with open("navigation.json", "r") as file:
+		f = json.load(file)
+		return f.get(room)
+
+def navigation_intent(room):
+    route = get_route(room)
+    if route is None:
+        return "Sorry I could not recognise that room, please try again"
+    else:
+        return route[0]
+
+class NavigationIntent(intent_base):
+	def action(self, intents):
+    slots = intents.to_dict().get("slots")
+    room = slots.get("Room").get("value").lower()
+
+    output_speech = navigation_intent(room)
+    print(output_speech)
+    self.push_to_notifier_speech(output_speech)
+    self.response = output_speech
+    self.user_input = "Asked for navigation to " + room
