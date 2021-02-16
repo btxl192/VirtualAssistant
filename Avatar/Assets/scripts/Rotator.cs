be93beb643341841ab2f09d512c7c321f91d27fa
brandon <btan192@gmail.com>
2021-02-16 11:19:43 +0000

    added rotator script

---
+++ Avatar/Assets/scripts/Rotator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator
+{
+
+    private static Dictionary<Transform, Quaternion> allRotations = new Dictionary<Transform, Quaternion>();
+
+    public static void Rotate(Transform t, Vector3 rotation)
+    {
+        if (!allRotations.ContainsKey(t))
+        {
+            allRotations.Add(t, t.rotation);
+        }
+        Vector3 newAngle = allRotations[t].eulerAngles + rotation;
+        SetRotation(t, newAngle);
+    }
+
+    public static void SetRotation(Transform t, Vector3 rotation)
+    {
+        if (!allRotations.ContainsKey(t))
+        {
+            allRotations.Add(t, t.rotation);
+        }
+        t.rotation = allRotations[t] = Quaternion.Slerp(allRotations[t], Quaternion.Euler(rotation), Time.deltaTime);
+    }
+}
