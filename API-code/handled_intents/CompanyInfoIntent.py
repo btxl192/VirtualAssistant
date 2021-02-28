3dfd009d42c74dbd3afb3dcfdae2832a244cc545
brandon <btan192@gmail.com>
2021-02-28 15:44:41 +0000

    fixed response

--- API-code/handled_intents/CompanyInfoIntent.py
+++ API-code/handled_intents/CompanyInfoIntent.py
@@ -11,14 +11,14 @@ class CompanyInfoIntent(intent_base):
         company_info = self.get_company_info(company)
         if company_info is None:
             self.emotion = "ASHAMED"
-            return "Sorry I could not recognise that company, please try again"
+            return "Sorry I could not recognise that company, please try again."
         if sector is None:
             return company_info.get("about")[0]
         elif sector in company_info.keys():
             return company_info.get(sector)[0]
         else:
             self.emotion = "ASHAMED"
-            return "Sorry I could not recognise that sector, please try again"
+            return "Sorry I could not recognise that sector, please try again."
 
     def action(self, handler_input):  
         #slots = handler_input.request_envelope.request.intent.to_dict().get("slots")        
@@ -30,9 +30,14 @@ class CompanyInfoIntent(intent_base):
         except (TypeError, AttributeError) as e:
             sector = None
     
-        self.response = self.company_info_intent(company, sector) + " What else would you like to know about " + company + "?"
+        self.response = self.company_info_intent(company, sector)
         self.user_input = "Asked for info about " + company
         
-        set_dismissal_msg(handler_input, "No more questions about " + company + " then.")
-        add_answer_intent(handler_input, "CompanyInfoMoreIntent")
-        set_sess_attr(handler_input, "current_company", company)
+        
+        if "Sorry" not in self.response:
+            self.response += " What else would you like to know about " + company + "?"
+            set_dismissal_msg(handler_input, "No more questions about " + company + " then.")
+            add_answer_intent(handler_input, "CompanyInfoMoreIntent")
+            set_sess_attr(handler_input, "current_company", company)
+        
+        
