025f3c33ee34286acebe4526e1431e8211d6ae05
brandon <btan192@gmail.com>
2021-03-09 14:09:33 +0000

    bug fix

--- AVSClientFiles/startapp.bat
+++ AVSClientFiles/startapp.bat
@@ -1,3 +1,5 @@
 if not exist .\build\bin\audioOutput\NUL mkdir .\build\bin\audioOutput
+echo 1 > ".\build\bin\alexaInput.txt"
+echo. >> ".\build\bin\alexaInput.txt"
 start startsample.bat
 start startaudiohost.bat
