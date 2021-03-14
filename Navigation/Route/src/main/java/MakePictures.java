c181af5cbc834ad9e32722178fdf0be162a99643
Kaloyan Rusev <kaloyan.rusev.19@ucl.ac.uk>
2021-03-14 18:36:11 +0200

    keep Blue file

--- Navigation/Route/src/main/java/MakePictures.java
+++ Navigation/Route/src/main/java/MakePictures.java
@@ -27,7 +27,7 @@ public class MakePictures implements Runnable{
             String picName = floor + "/" + room + "Pic.png";
             String mp4VidName = floor + "/" + room + "Vid.mp4";
             String webmVidName = floor + "/" + room + ".webm";
-            Process p = Runtime.getRuntime().exec("ffmpeg -r 1/5 -i " + picName + " -c:v libx264 -r 1/7 -pix_fmt yuv420p " + mp4VidName);
+            Process p = Runtime.getRuntime().exec("ffmpeg -r 1/5 -i " + picName + " -c:v libx264 -r 1/5 -pix_fmt yuv420p " + mp4VidName);
             p.waitFor();
             p = Runtime.getRuntime().exec("ffmpeg -i " + mp4VidName + " -c:v libvpx -b:v 1M -c:a libvorbis " + webmVidName);
             p.waitFor();
