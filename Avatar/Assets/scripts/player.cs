bf4fddb1e42f76e802faa14caa8a0ead6681ffeb
brandon <btan192@gmail.com>
2021-03-15 18:16:09 +0000

    fixed

--- Avatar/Assets/scripts/player.cs
+++ Avatar/Assets/scripts/player.cs
@@ -10,8 +10,8 @@ public class player : MonoBehaviour
     public Animator animator;
     public static GameObject activeModel { get; private set; }
     public GameObject neck;
-<<<<<<< HEAD
     public FaceTracker cameraFaceTracker;
+    private UnityEngine.Video.VideoPlayer videoPlayer;
 
     private int y_rotation = 0;
     private int prev_rotation = 0;
@@ -32,6 +32,7 @@ public class player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        videoPlayer = GameObject.Find("Video Player").GetComponent<UnityEngine.Video.VideoPlayer > ();
         animator = GetComponent<Animator>();
         cameraFaceTracker = GameObject.Find("RawImage").GetComponent<FaceTracker>();
         GameObject main_camera = GameObject.Find("Main Camera");
@@ -45,48 +46,7 @@ public class player : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        y_rotation = cameraFaceTracker.rotation;
-        if (y_rotation != activeModel.transform.rotation.eulerAngles.y)
-        {
-
-            int unityAngle = System.Convert.ToInt32(activeModel.transform.rotation.eulerAngles.y);
-            
-            int neckAngle = System.Convert.ToInt32(Rotator.Rotate(neck.transform, new Vector3(0f, 0f, 0f)).y * 50);
-
-            if (unityAngle > 180)
-            {
-                unityAngle = unityAngle - 360;
-            }
-
-            if (neckStartAngle != 0)
-=======
-    public FaceTracker cameraFaceTracker;
-    private UnityEngine.Video.VideoPlayer videoPlayer;
-
-    private int y_rotation = 0;
-    private int prev_rotation = 0;
-    private int neckStartAngle;
-
-    public bool faceDetected = false;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        videoPlayer = GameObject.Find("Video Player").GetComponent<UnityEngine.Video.VideoPlayer > ();
-        animator = GetComponent<Animator>();
-        cameraFaceTracker = GameObject.Find("RawImage").GetComponent<FaceTracker>();
-        GameObject main_camera = GameObject.Find("Main Camera");
-        neckStartAngle = System.Convert.ToInt32(Rotator.Rotate(neck.transform, new Vector3(0f, 0f, 0f)).y * 50);
-        faceDetected = cameraFaceTracker.faceDetected;
-    }
-    private void Update()
-    {
-        faceDetected = cameraFaceTracker.faceDetected;
-    }
-    // Update is called once per frame
-    void LateUpdate()
-    {
-        int unityAngle = System.Convert.ToInt32(unityChan.transform.rotation.eulerAngles.y);
+        int unityAngle = System.Convert.ToInt32(activeModel.transform.rotation.eulerAngles.y);
 
         if (unityAngle > 180)
         {
@@ -96,31 +56,23 @@ public class player : MonoBehaviour
         if (videoPlayer.isPlaying)
         {
             if (unityAngle > -40)
->>>>>>> aeb84502a5aa954c1fd3fdcbc67fbfbb7c813bf5
             {
-                Rotator.Rotate(unityChan.transform, new Vector3(0f, -25f, 0f));
+                Rotator.Rotate(activeModel.transform, new Vector3(0f, -25f, 0f));
             }
         }
         else
         {
             y_rotation = cameraFaceTracker.rotation;
-            if (y_rotation != unityChan.transform.rotation.eulerAngles.y)
+            if (y_rotation != activeModel.transform.rotation.eulerAngles.y)
             {
                 int neckAngle = System.Convert.ToInt32(Rotator.Rotate(neck.transform, new Vector3(0f, 0f, 0f)).y * 50);
 
                 
 
-<<<<<<< HEAD
-            if (y_rotation > neckAngle)
-            {
-                Rotator.Rotate(neck.transform, new Vector3(0f, 25f, 0f));
-            }
-=======
                 if (neckStartAngle != 0)
                 {
                     neckAngle -= neckStartAngle;
                 }
->>>>>>> aeb84502a5aa954c1fd3fdcbc67fbfbb7c813bf5
 
                 if (y_rotation > neckAngle)
                 {
@@ -136,32 +88,18 @@ public class player : MonoBehaviour
                 }
             
 
-<<<<<<< HEAD
-            if ((y_rotation >= 6 || unityAngle != 0) && (unityAngle < y_rotation))
-            {
-                Rotator.Rotate(activeModel.transform, new Vector3(0f, 25f, 0f));
-            }
-
-            else if ((y_rotation <= -6 || unityAngle != 0) && (unityAngle > y_rotation))
-            {
-                Rotator.Rotate(activeModel.transform, new Vector3(0f, -25f, 0f));
-            }
-
-        }
-=======
                 if ((y_rotation >= 6 || unityAngle != 0) && (unityAngle < y_rotation))
                 {
-                    Rotator.Rotate(unityChan.transform, new Vector3(0f, 25f, 0f));
+                    Rotator.Rotate(activeModel.transform, new Vector3(0f, 25f, 0f));
                 }
 
                 else if ((y_rotation <= -6 || unityAngle != 0) && (unityAngle > y_rotation))
                 {
-                    Rotator.Rotate(unityChan.transform, new Vector3(0f, -25f, 0f));
+                    Rotator.Rotate(activeModel.transform, new Vector3(0f, -25f, 0f));
                 }
 
             }
         }
 
->>>>>>> aeb84502a5aa954c1fd3fdcbc67fbfbb7c813bf5
     }
 }
