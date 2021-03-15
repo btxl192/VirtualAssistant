61e1d68e2ad70ad93a2b83a6ee0436b98e9b6576
Kaloyan Rusev <kaloyan.rusev.19@ucl.ac.uk>
2021-03-15 21:24:36 +0200

    navigation intent

--- API-code/handled_intents/NavigationIntent.py
+++ API-code/handled_intents/NavigationIntent.py
@@ -1,10 +1,17 @@
 from .intent_base import *
 import json
 
-def get_route(room):
-    with open("navigation.json", "r") as file:
+with open("navigation.json", "r") as file:
         f = json.load(file)
-        return f.get(room)
+
+def get_floors():
+        return list(f.keys())
+
+def get_blue_floor():
+    return f.get("Blue")
+
+def get_rooms(floor):
+    return list(f.get(floor))
 
 def navigation_intent(room):
     route = get_route(room)
@@ -18,7 +25,31 @@ class NavigationIntent(intent_base):
         #slots = intents.to_dict().get("slots")
         #room = slots.get("Room").get("value").lower()
         room = get_slot_value(handler_input, "Room").lower()
-        output_speech = navigation_intent(room)
-        print(output_speech)
-        self.response = output_speech
+        blueFloor = get_blue_floor()
+    if room in get_rooms(str(blueFloor)):
+        self.response = f.get(str(blueFloor)).get(room)[0]
         self.user_input = "Asked for navigation to " + room
+        self.add_unity_msg("NavRoom", room)
+        return
+    floors = get_floors()
+    for i in range (1, len(floors) - 1):
+        if (blueFloor + i) in floors:
+            if room in get_rooms(blueFloor + i):
+                output_speech = "Take the lift to " str(blueFloor + i) +"th floor. "
+                otput_speech += f.get(str(blueFLoor)).get("lift")[0] + " then "
+                output_speech += f.get(str(blueFloor) + i).get(room)[0]
+                self.response = output_speech
+                self.user_input = "Asked for navigation to " + room
+                self.add_unity_msg("NavRoom", room)
+                self.add_unity_msg("NavFloor", blueFloor + i)
+                return
+        if (blueFloor - i) in floors:
+            if room in get_rooms(blueFloor - i):
+                output_speech = "Take the lift to " str(blueFloor - i) +"th floor. "
+                otput_speech += f.get(str(blueFLoor)).get("lift")[0] + " then "
+                output_speech += f.get(str(blueFloor) + i).get(room)[0]
+                self.response = output_speech
+                self.user_input = "Asked for navigation to " + room
+                self.add_unity_msg("NavRoom", room)
+                self.add_unity_msg("NavFloor", blueFloor - i)
+                
