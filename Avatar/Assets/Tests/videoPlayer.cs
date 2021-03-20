e0ce164d18a856b78f26a949fe4816a34dcbc529
maowtm <gh-on-windows@maowtm.org>
2021-03-20 02:57:48 +0000

    Unit testing bootstrap

---
+++ Avatar/Assets/Tests/videoPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class videoPlayer
+    {
+        [UnityTest]
+        public IEnumerator videoPlayerWithEnumeratorPasses()
+        {
+            GameObject vp = GameObject.Find("Video Player");
+            var vpcom = vp.GetComponent<videoPlayerScript>();
+            yield return null;
+        }
+    }
+}
